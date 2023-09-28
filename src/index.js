fc8dff869cfede363851562b4c644f373a013270
basima <bsom_ba@yahoo.com>
2023-09-28 13:45:33 +0200

    React Profile Card

--- src/index.js
+++ src/index.js
@@ -38,31 +38,33 @@ function App() {
   const info = {
     imgSrc: "ahmad.jpg",
     title: "Ahmad Almasri",
-    dscription: `je suis une développeur web junior à la recherche d'un emploi en télétravail
+    description: `je suis une développeur web junior à la recherche d'un emploi en télétravail
   au sein d'une agence web.J'apprends activement de nouvelles technologies.`,
   };
 
   return (
-    <div className="card">
+    <Card>
       <Avatar photo={info.imgSrc} name="avatar" />
-      <div className="data">
-        <Intro title={info.title} discription={info.dscription} />
-        {dataSkills.map((skill) => (
-          <Skills skillObj={skill} key={skill.skill} />
-        ))}
-      </div>
-    </div>
+      <CardData>
+        <Intro title={info.title} description={info.description} />
+        <List>
+          {dataSkills.map((skill) => (
+            <Skills skillObj={skill} key={skill.skill} />
+          ))}
+        </List>
+      </CardData>
+    </Card>
   );
 }
-function Avatar(props) {
-  return <img src={props.photo} className="avatar" alt={props.name} />;
+function Avatar({ photo, name }) {
+  return <img src={photo} className="avatar" alt={name} />;
 }
-function Intro(props) {
+function Intro({ title, description }) {
   return (
-    <div>
-      <h1>{props.title}</h1>
-      <p>{props.discription}</p>
-    </div>
+    <CardIntro>
+      <h1>{title}</h1>
+      <p>{description}</p>
+    </CardIntro>
   );
 }
 function Skills({ skillObj }) {
@@ -72,17 +74,32 @@ function Skills({ skillObj }) {
   };
 
   return (
-    <div>
-      <ul className="skill-list">
-        <li className="skill" style={{ backgroundColor: skillObj.color }}>
-          {skillObj.skill}
-          {level?.[skillObj?.level] || "👶"}
-        </li>
-      </ul>
-    </div>
+    <Item style={{ backgroundColor: skillObj.color }}>
+      {skillObj.skill}
+      {level?.[skillObj?.level] || "👶"}
+    </Item>
   );
 }
+function Card({ children }) {
+  return <div className="card">{children}</div>;
+}
 
+function CardData({ children }) {
+  return <div className="data">{children}</div>;
+}
+function CardIntro({ children }) {
+  return <div>{children}</div>;
+}
+function List({ children }) {
+  return <ul className="skill-list">{children}</ul>;
+}
+function Item({ children, style }) {
+  return (
+    <li className="skill" style={style}>
+      {children}
+    </li>
+  );
+}
 const root = ReactDom.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
